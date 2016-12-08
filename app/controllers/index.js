0afa5fd69929164e2856ac7806cb662c4e32aef2
Unknown <damian.kropka@gmail.com>
2016-12-08 16:54:14 +0100

    Password field now clears itself

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   game: Ember.inject.service(),
-  actions:{
-    startGame(){
+  actions: {
+    startGame() {
       this.get('game').setup(this.get('pass').toUpperCase());
+      this.set('pass', '');
       this.transitionToRoute('game');
     }
   }
