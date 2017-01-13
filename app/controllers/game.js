6bd92f643792a4cf7695b8383d1467391112b1ba
Damian Suchy <damian.kropka@gmail.com>
2017-01-13 11:00:13 +0000

    Fixed win condition & further refactoring

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -6,19 +6,20 @@ export default Ember.Controller.extend({
   hp: 100 - (10),
   win: false,
 
-  // filteredPassword: Ember.computed.map('password.[]',(letter) => this.get('playerChoices').includes(letter) ? letter : '-')
-
   filteredPassword: Ember.computed('game.password', 'playerChoices.[]', function () {
     let password = this.get('game.password').split(''),
+      letters = this.get('playerChoices'),
       alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUVWXYZŻŹ".split('');
-    return password.map((letter) => this.get('playerChoices').includes(letter) || !alphabet.includes(letter) || letter == ' ' ? letter : '-').join('');
+
+    return password.map((letter) => letters.includes(letter) || !alphabet.includes(letter) || letter == ' ' ? letter : '-').join('');
   }),
 
   isTheWinner: Ember.computed('game.password', 'playerChoices.[]', function () {
     let pass = this.get('game.password'),
-      letters = this.get('playerChoices');
+      letters = this.get('playerChoices'),
+      alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUVWXYZŻŹ".split('');
 
-    return pass.split('').every(letter => letters.includes(letter));
+    return pass.split('').every(letter => letters.includes(letter) || !alphabet.includes(letter) || letter == ' ');
   })
 
 });
